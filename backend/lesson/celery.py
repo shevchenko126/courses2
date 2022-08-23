2ba55034e7316565d43365b29923e2198abb6b55
shevchenko126 <dima.shevchenkooo@gmail.com>
2022-08-23 19:56:57 +0300

    Update celery.py

--- backend/lesson/celery.py
+++ backend/lesson/celery.py
@@ -1,4 +1,7 @@
+import os
 from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lesson.settings')
 
 app = Celery('lesson')
 app.config_from_object('django.conf:settings', namespace='CELERY')
